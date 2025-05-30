e14f490c6b08d5a2e37bde9b2c359cbd4f15d2c4
jas.bali <bali0019@gmail.com>
2025-05-30 13:36:22 -0400

    Removed gitignore related files

--- rendered_project_testing/resources/databricks/tesseract_install_init_script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo rm -rf /var/cache/apt/archives/* /var/lib/apt/lists/* && \
-sudo apt-get clean && \
-sudo apt-get update && \
-sudo apt-get install poppler-utils tesseract-ocr -y
